76ef9f9205ae6d90e5eb1442d72a994caa0e2511
Tsolak <dcntsolak@yahoo.com>
2024-01-22 17:39:51 +0400

    Add Lesson3:Task3

---
+++ Lesson3/Task3/Program.cs
@@ -0,0 +1,24 @@
+﻿//Создадим функцию, который каким-то образом
+//будет изменять элементы массива.
+
+void ZeroEvenElements(int[] arr) //Скажем хотим обнулить элементы массива;
+{
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (i % 2 == 0)
+        {
+            arr[i] = 0;
+        }
+    }
+}
+void PrintArray(int[] arr)
+{
+    foreach (int e in arr)
+    {
+        Console.Write($"{e} ");
+    }
+}
+
+int[] array = { 1, 2, 3, 4, 5 };
+ZeroEvenElements(array);
+PrintArray(array);
